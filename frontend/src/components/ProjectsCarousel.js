eb194d33d7b1eaf792a2ef3c7356eee39b079aed
Griffers <sgriff09@icloud.com>
2024-10-01 10:01:54 -0400

    fixed project view to point to the right api

--- frontend/src/components/ProjectsCarousel.js
+++ frontend/src/components/ProjectsCarousel.js
@@ -11,7 +11,7 @@ const ProjectsCarousel = () => {
   useEffect(() => {
     const fetchProjects = async () => {
       try {
-        const response = await fetch(`http://localhost:5000/api/projects`);
+        const response = await fetch(`${process.env.REACT_APP_BACKEND_URL_PROD}/api/projects`);
         if (!response.ok) {
           throw new Error('Failed to fetch projects');
         }
